31c83ab99d58c23deeb94aa22baed1735b09e667
Tuomo Tanskanen <tumi@tumi.fi>
2014-03-03 10:54:11 +0200

    Do not install postfix by default

--- scripts/install-apache.sh
+++ scripts/install-apache.sh
@@ -24,9 +24,12 @@ a2ensite kisakone
 service apache2 restart
 
 # postfix
-echo "postfix postfix/main_mailer_type select Internet Site" | debconf-set-selections
-echo "postfix postfix/mailname string precise32" | debconf-set-selections
-apt-get -y install postfix
+# do not install postfix by default on a development box to avoid mailing people by mistake
+if false; then
+  echo "postfix postfix/main_mailer_type select Internet Site" | debconf-set-selections
+  echo "postfix postfix/mailname string precise32" | debconf-set-selections
+  apt-get -y install postfix
+fi
 
 # use sample configs for quick startup
 cp /kisakone/config_site.php.sample /kisakone/config_site.php
