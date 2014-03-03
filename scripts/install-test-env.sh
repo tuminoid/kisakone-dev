53ca7947e35349deb60a2b0e951f449132bcf605
Tuomo Tanskanen <tumi@tumi.fi>
2014-03-03 10:54:23 +0200

    Install php-cs-fixer and a 'kisakone-fix-cs' command to fix all files

--- scripts/install-test-env.sh
+++ scripts/install-test-env.sh
@@ -3,12 +3,46 @@
 SELENIUM="selenium-server-standalone-2.39.0.jar"
 CACHE="/vagrant/.cache"
 
-# nodejs
+
+# prime cache
+mkdir -p $CACHE
 apt-get -y update
+
+
+# install php-cs-fixer
+apt-get -y install wget
+wget -nc http://cs.sensiolabs.org/get/php-cs-fixer.phar -O $CACHE/php-cs-fixer
+mkdir -p /usr/local/bin
+cp $CACHE/php-cs-fixer /usr/local/bin/
+chmod 755 /usr/local/bin/php-cs-fixer
+cat <<EOF >/usr/local/bin/kisakone-fix-cs
+#!/bin/bash
+
+DIR=/kisakone
+if [ \$# -gt 0 ]; then
+  DIR=\$1
+  php-cs-fixer fix \$DIR --fixers=-visibility
+else
+  # fix code dirs (not Smarty etc)
+  for D in core data inputhandlers ui; do
+    php-cs-fixer fix \$DIR/\$D --fixers=-visibility
+  done
+
+  # fix files
+  for F in helpers.php index.php inputmapping.php sfl_integration.php; do
+    php-cs-fixer fix \$DIR/\$F --fixers=-visibility
+  done
+fi
+EOF
+chmod 755 /usr/local/bin/kisakone-fix-cs
+
+
+# nodejs
 apt-get -y install python-software-properties python g++ make
 add-apt-repository -y ppa:chris-lea/node.js
 apt-get -y update
 apt-get -y install nodejs
+
 
 # nightwatch.js
 apt-get -y install git
@@ -24,7 +58,7 @@ apt-get -y install openjdk-7-jre-headless
 
 # selenium, with little caching
 apt-get -y install wget
-mkdir -p /usr/lib/selenium $CACHE
+mkdir -p /usr/lib/selenium
 (cd $CACHE && wget -q -nc https://selenium.googlecode.com/files/$SELENIUM)
 cp $CACHE/$SELENIUM /usr/lib/selenium/
 cat <<EOF >/etc/init.d/selenium
@@ -101,11 +135,13 @@ service selenium start
 
 
 # kisakone tests
-cat <<EOF >/usr/bin/kisakone-run-tests
+cat <<EOF >/usr/local/bin/kisakone-run-tests
 #!/bin/bash
 
 cd /vagrant/tests
 /root/nightwatch/bin/nightwatch -c nightwatch-settings.json $@
 EOF
-chmod 755 /usr/bin/kisakone-run-tests
-echo "Test environment setup done! Run Kisakone UI test suite with 'kisakone-run-tests [category]'!"
+chmod 755 /usr/local/bin/kisakone-run-tests
+echo "Test environment setup done!"
+echo "  Run Kisakone UI test suite with 'kisakone-run-tests [category]'!"
+echo "  Fix coding style before committing with 'kisakone-fix-cs [dir]'!"
