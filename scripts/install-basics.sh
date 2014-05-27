4b7a92773ed8d785596c533579ef17288eb29497
Tuomo Tanskanen <tumi@tumi.fi>
2014-05-27 16:27:39 +0300

    Fixes to basics

--- scripts/install-basics.sh
+++ scripts/install-basics.sh
@@ -2,11 +2,15 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
+# use finnish mirrors
+sed -i -e 's,us.archive.ubuntu.com,fi.archive.ubuntu.com,g' /etc/apt/sources.list
+sed -i -e 's,http://archive.ubuntu.com,http://fi.archive.ubuntu.com,g' /etc/apt/sources.list
+
 # update repos
 apt-get -y update
 
 # basics
-apt-get -y install git nano curl wget nmap command-not-found man software-properties-common
+apt-get -y install git nano less curl wget nmap command-not-found man software-properties-common
 
 # 12.04 has python-software-properties for add-apt-repository,
 # which 14.04 handles with software-properties-common
