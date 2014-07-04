15defadefc3dee6cf97eaf0e1e0def0b1ce17941
Tuomo Tanskanen <tuomo@tanskanen.org>
2014-07-04 08:57:00 +0300

    Add php5-curl

--- scripts/install-apache.sh
+++ scripts/install-apache.sh
@@ -4,7 +4,7 @@
 export DEBIAN_FRONTEND=noninteractive
 
 # apache
-apt-get -y install apache2 libapache2-mod-php5 libapache2-mod-auth-mysql php5-mysql php5-memcache
+apt-get -y install apache2 libapache2-mod-php5 libapache2-mod-auth-mysql php5-mysql php5-memcache php5-curl
 echo "ServerName localhost" > /etc/apache2/conf.d/fqdn
 a2enmod php5
 a2enmod rewrite
