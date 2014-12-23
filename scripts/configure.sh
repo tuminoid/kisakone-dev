9f1419c79320f8321a1afc7ec098decec01986ca
Tuomo Tanskanen <tuomo@tanskanen.org>
2014-12-23 08:09:56 +0200

    rename

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -2,14 +2,14 @@
 
 # use sample configs for quick startup
 if [ ! -e /kisakone/config_site.php ]; then
-  cp /kisakone/config_site.php.sample /kisakone/config_site.php
+  cp /kisakone/config_site.php.example /kisakone/config_site.php
 fi
 
 # restore backups (expects backup file to contain proper "use database;")
 if [ -e "/vagrant/kisakone.sql.backup" ]; then
   echo "Restoring backup SQL from: kisakone.sql.backup"
   mysql -u root --password=pass < /vagrant/kisakone.sql.backup
-  cp /kisakone/config.php.sample /kisakone/config.php
+  cp /kisakone/config.php.example /kisakone/config.php
   DB=$(cat /vagrant/kisakone.sql.backup | grep "Current Database" | grep kisakone | grep -v "next" | grep -v "test" | head -1 | cut -f2 -d'`' | cut -f1 -d'`')
   if [ "$DB" != "" ]; then
     echo "Detected DB name: $DB - altering config.php for you ..."
