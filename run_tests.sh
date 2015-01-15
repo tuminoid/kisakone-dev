432124176f85718295891b0db001a0e181d5f29e
Tuomo Tanskanen <tuomo@tanskanen.org>
2015-01-15 13:22:29 +0200

    move admin login to its own group

--- run_tests.sh
+++ run_tests.sh
@@ -30,17 +30,17 @@ check_syntax() {
 while [[ $1 ]]; do
   case $1 in
     unit|unittests) check_syntax unittests; exec ./tools/run_unittests.sh ;;
+    syntax) check_syntax ../kisakone ;;
 
-    install) TESTS="$TESTS --clean install" ;;
+    install) TESTS="$TESTS --clean install admin" ;;
     user) TESTS="$TESTS --clean install user_creation user_profile" ;;
     smoke) TESTS="$TESTS user_profile" ;;
-    all) ./tools/run_unittests.sh; TESTS=; break ;;
+    all) check_syntax ../kisakone; ./tools/run_unittests.sh; TESTS=; break ;;
     -h|--help|help|*) usage ;;
   esac
   shift
 done
 
-check_syntax ../kisakone
 vagrant ssh -- "sudo rm -f /var/log/apache2/error_local.log; sudo service apache2 restart"
 vagrant ssh -- "sudo /vagrant/tools/kisakone-run-tests $TESTS"
 vagrant ssh -- "sudo cat /var/log/apache2/error_local.log"
