936ff3f0b83c97e1b35c170a24fd8d1cf6ba138a
Tuomo Tanskanen <tuomo@tanskanen.org>
2015-01-15 10:13:23 +0200

    check syntax more specifically

--- run_tests.sh
+++ run_tests.sh
@@ -8,22 +8,28 @@ usage() {
 }
 
 check_syntax() {
+  DIR=$1
   which php >/dev/null || return 0
 
-  for PHP in $(find ../kisakone -name '*.php'); do
+
+  echo -n "syntax check: "
+  for PHP in $(find $DIR -name '*.php'); do
+    if echo $PHP | grep -q "Smarty/templates"; then
+      continue
+    fi
+
+    echo -n "."
     if ! php -l $PHP >/dev/null; then
       php -l $PHP
       exit 1
     fi
   done
+  echo " : ok"
 }
-
-clear
-check_syntax
 
 while [[ $1 ]]; do
   case $1 in
-    unit|unittests) exec ./tools/run_unittests.sh ;;
+    unit|unittests) check_syntax unittests; exec ./tools/run_unittests.sh ;;
 
     install) TESTS="$TESTS --clean install" ;;
     user) TESTS="$TESTS --clean install user_creation user_profile" ;;
@@ -34,6 +40,7 @@ while [[ $1 ]]; do
   shift
 done
 
+check_syntax ../kisakone
 vagrant ssh -- "sudo rm -f /var/log/apache2/error_local.log; sudo service apache2 restart"
 vagrant ssh -- "sudo /vagrant/tools/kisakone-run-tests $TESTS"
 vagrant ssh -- "sudo cat /var/log/apache2/error_local.log"
