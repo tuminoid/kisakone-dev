41af25f90e38b182ed9b7b3173b62f1b4561e54f
Tuomo Tanskanen <tuomo@tanskanen.org>
2015-01-15 09:00:50 +0200

    more login tests

--- unittests/testLogin.php
+++ unittests/testLogin.php
@@ -2,19 +2,119 @@
 
 require_once '../../kisakone/core/login.php';
 
-class CryptoTest extends PHPUnit_Framework_TestCase
+class LoginTest extends PHPUnit_Framework_TestCase
 {
   private $testhash;
 
   public function setUp() {
-     $this->testhash = md5("foobar");
+     $this->testhash = md5("foobar12");
   }
 
-  /*
-   * Test GenerateHash with "md5"
+
+  /**
+   * Test valid IsValidPassword cases
+   */
+  public function test_valid_IsValidPassword() {
+    $this->assertEquals(
+      true,
+      IsValidPassword("12345678")
+    );
+
+    $this->assertEquals(
+      true,
+      IsValidPassword("12345678abcdefgh")
+    );
+
+    $this->assertEquals(
+      true,
+      IsValidPassword("1234567890123456789012345678901234567890")
+    );
+  }
+
+
+  /**
+   * Test valid IsValidPassword cases
+   */
+  public function test_invalid_IsValidPassword() {
+    $this->assertEquals(
+      false,
+      IsValidPassword("")
+    );
+
+    $this->assertEquals(
+      false,
+      IsValidPassword(null)
+    );
+
+    $this->assertEquals(
+      false,
+      IsValidPassword(false)
+    );
+
+    $this->assertEquals(
+      false,
+      IsValidPassword("1234567")
+    );
+
+    $this->assertEquals(
+      false,
+      IsValidPassword("12345678901234567890123456789012345678901")
+    );
+  }
+
+
+  /**
+   * Test valid IsValidSalt cases
+   */
+  public function test_valid_IsValidSalt() {
+    $this->assertEquals(
+      true,
+      IsValidSalt("12345678901234567890123456789012")
+    );
+  }
+
+
+  /**
+   * Test invalid IsValidSalt cases
+   */
+  public function test_invalid_IsValidSalt() {
+    $this->assertEquals(
+      false,
+      IsValidSalt("1234567890123456789012345678901")
+    );
+
+    $this->assertEquals(
+      false,
+      IsValidSalt("123456789012345678901234567890123")
+    );
+
+    $this->assertEquals(
+      false,
+      IsValidSalt(null)
+    );
+
+    $this->assertEquals(
+      false,
+      IsValidSalt(false)
+    );
+
+    $this->assertEquals(
+      false,
+      IsValidSalt(true)
+    );
+
+    $this->assertEquals(
+      false,
+      IsValidSalt(-1)
+    );
+  }
+
+
+  /**
+   * Test valid GenerateHash with "md5"
    */
   public function test_MD5() {
-    $hash = GenerateHash("foobar");
+    $hash = GenerateHash("foobar12");
 
     $this->assertNotEquals(
       null,
@@ -33,19 +133,23 @@ class CryptoTest extends PHPUnit_Framework_TestCase
 
     $this->assertEquals(
       $this->testhash,
-      GenerateHash("foobar", "md5")
+      GenerateHash("foobar12", "md5")
     );
 
     $this->assertEquals(
       $this->testhash,
-      GenerateHash("foobar", "md5", "12345678")
+      GenerateHash("foobar12", "md5", "12345678")
     );
   }
 
+
+  /**
+   * Test invalid GenerateHash with "md5"
+   */
   public function test_invalid_MD5() {
     $this->assertNotEquals(
       $this->testhash,
-      GenerateHash("barbaz")
+      GenerateHash("barbaz12")
     );
 
     $this->assertEquals(
@@ -65,8 +169,8 @@ class CryptoTest extends PHPUnit_Framework_TestCase
   }
 
 
-  /*
-   * Test GenerateSalt
+  /**
+   * Test valid GenerateSalt
    */
   public function test_Salt_creation() {
     $salt = GenerateSalt();
@@ -89,11 +193,11 @@ class CryptoTest extends PHPUnit_Framework_TestCase
 
 
   /*
-   * Test GenerateHash with "crypt"
+   * Test valid GenerateHash with "crypt"
    */
   public function test_simple_Crypt() {
     $salt = GenerateSalt();
-    $crypt = GenerateHash("foobar", "crypt", $salt);
+    $crypt = GenerateHash("foobar12", "crypt", $salt);
 
     $this->assertNotEquals(
       null,
@@ -111,14 +215,18 @@ class CryptoTest extends PHPUnit_Framework_TestCase
     );
 
     $salt = 't4DyD8LupAXVJ9TEYknt4jQnzIHyDw==';
-    $expected = '$2y$10$t4DyD8LupAXVJ9TEYknt4eCkepEqkDCjTbdx.wAcv7AAjBW4Wkg6a';
+    $expected = '$2y$10$t4DyD8LupAXVJ9TEYknt4ergTKZNren6Rkn.DfYM7L9eaW5E/PDJi';
     $this->assertEquals(
       $expected,
-      GenerateHash("foobar", "crypt", $salt)
+      GenerateHash("foobar12", "crypt", $salt)
     );
 
   }
 
+
+  /**
+   * Test invalid GenerateHash with "crypt"
+   */
   public function test_invalid_Crypt() {
     $salt = GenerateSalt();
 
@@ -134,27 +242,27 @@ class CryptoTest extends PHPUnit_Framework_TestCase
 
     $this->assertEquals(
       null,
-      GenerateHash("foobar", "crypt")
+      GenerateHash("foobar12", "crypt")
     );
 
     $this->assertEquals(
       null,
-      GenerateHash("foobar", "crypt", "")
+      GenerateHash("foobar12", "crypt", "")
     );
 
     $this->assertEquals(
       null,
-      GenerateHash("foobar", "crypt", null)
+      GenerateHash("foobar12", "crypt", null)
     );
 
     $this->assertEquals(
       null,
-      GenerateHash("foobar", "crypt", 1)
+      GenerateHash("foobar12", "crypt", 1)
     );
 
     $this->assertEquals(
       null,
-      GenerateHash("foobar", "crypt", -1)
+      GenerateHash("foobar12", "crypt", -1)
     );
 
     $this->assertEquals(
@@ -185,27 +293,27 @@ class CryptoTest extends PHPUnit_Framework_TestCase
 
 
   /*
-   * Test GenerateHash with invalid "crypt"
+   * Test GenerateHash with invalid "crypt" calls
    */
   public function test_invalid_calls() {
     $this->assertEquals(
       null,
-      GenerateHash("foobar", null)
+      GenerateHash("foobar12", null)
     );
 
     $this->assertEquals(
       null,
-      GenerateHash("foobar", "")
+      GenerateHash("foobar12", "")
     );
 
     $this->assertEquals(
       null,
-      GenerateHash("foobar", "rc4")
+      GenerateHash("foobar12", "rc4")
     );
 
     $this->assertEquals(
       null,
-      GenerateHash("foobar", -1)
+      GenerateHash("foobar12", -1)
     );
   }
 }
