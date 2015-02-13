2c05d0b6466a69fa255b16f6a9472092b483cace
Tuomo Tanskanen <tuomo@tanskanen.org>
2015-02-13 10:02:01 +0200

    make ntp more aggressive

--- scripts/install-basics.sh
+++ scripts/install-basics.sh
@@ -10,7 +10,11 @@ sed -i -e 's,http://archive.ubuntu.com,http://fi.archive.ubuntu.com,g' /etc/apt/
 apt-get -y update
 
 # basics
-apt-get -y install git nano less curl wget nmap command-not-found man software-properties-common
+apt-get -y install git nano less curl wget nmap command-not-found man software-properties-common ntp
+
+# make ntp more aggressive
+sed -i -e 's,server 0.ubuntu.pool.ntp.org,server 0.ubuntu.pool.ntp.org iburst,' /etc/ntp.conf
+service ntp restart
 
 # 12.04 has python-software-properties for add-apt-repository,
 # which 14.04 handles with software-properties-common
